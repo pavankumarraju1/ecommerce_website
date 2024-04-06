54e700438e89f47b84a23da20d9f1904a2acdd52
pavan <999pavankumarraju@gmail.com>
2024-04-06 22:32:07 +0530

    done

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ server.use(express.static(path.resolve(__dirname,process.env.PUBLIC_DIR)));
 server.use(cors()); 
 server.use(express.json()); 
 server.use('/product',routes.router);
-//if we have given links in font end those will not be identifies in backend so use this to resolve the solution
-//By using * --> this is like an wildcard entry whenever we are trying to find any page that we are given routes in fronend /addCard or /getForm first it will checks at backend side and then if it is not there it will serch in the path where we have provided in path.resolvr __dirname...........
+// if we have given links in font end those will not be identifies in backend so use this to resolve the solution
+// By using * --> this is like an wildcard entry whenever we are trying to findany page that we are given routes in fronend /addCard or /getForm first it will checks at backend side and then if it is not there it will serch in the path where we have provided in path.resolvr __dirname...........
 server.use('*', (req, res) => {
     //this will somewhere creates errors in different os like windows and linux so its better to use path
     //res.sendFile(__dirname+'/build/index.html')
